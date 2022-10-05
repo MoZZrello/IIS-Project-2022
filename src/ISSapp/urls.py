10e26af18467cf7286fa2344fbf07c277b103131
Terka <tete.98765@gmail.com>
2022-10-05 11:39:49 +0200

    Frontend

--- src/ISSapp/urls.py
+++ src/ISSapp/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-]
+    #mozno potom zmazat
+    path('about', views.about, name="about"),
+    path('contact', views.concact, name="contact"),
+] 
--- src/ISSapp/views.py
+++ src/ISSapp/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
+from django.template import loader
+from django.shortcuts import render
 
 
-def index(request):
-    return HttpResponse("TEST")
+def index(request: HttpRequest) -> HttpResponse:
+    return render(request, 'index.html')
+
+def about(request: HttpRequest) -> HttpResponse:
+    return render(request, 'about.html')
+
+def concact(request: HttpRequest) -> HttpResponse:
+    return render(request, 'contact.html')
