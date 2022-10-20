76e918e5ae021f7b6862000198fa3766f8a00184
Richard Harman <jordan28031@gmail.com>
2022-10-20 10:57:47 +0200

    About Page filter changes

--- src/IISapp/filters.py
+++ src/IISapp/filters.py
@@ -1,16 +1,16 @@
 import django_filters
-from django_filters import DateFilter, CharFilter
+from django_filters import DateFilter, CharFilter, NumberFilter
 
 from .models import *
 
 
 class AnimalFilter(django_filters.FilterSet):
-    capture_date_after = DateFilter(field_name="capture_date", lookup_expr="gte")
-    capture_date_before = DateFilter(field_name="capture_date", lookup_expr="lt")
-    animal_name = CharFilter(field_name="animal_name", lookup_expr="icontains")
-    species = CharFilter(field_name="species", lookup_expr="icontains")
-    breed = CharFilter(field_name="breed", lookup_expr="icontains")
+    animal_name = CharFilter(field_name="animal_name", lookup_expr="icontains", label="Jméno mazlíčka")
+    species = CharFilter(field_name="species", lookup_expr="icontains", label="Druh")
+    breed = CharFilter(field_name="breed", lookup_expr="icontains", label="Plemeno")
+    age = NumberFilter(field_name="age", lookup_expr="exact", label="Věk")
 
     class Meta:
         model = Animal
         fields = ['animal_name', 'species', 'breed', 'age']
+
